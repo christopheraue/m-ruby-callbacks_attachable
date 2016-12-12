61ad29c276cd7cdfb8d0814d6ca1afef81ceea9d
Christopher Aue <mail@christopheraue.net>
2016-12-12 20:04:36 +0100

    added method aliases

--- mrblib/callbacks_attachable.rb
+++ mrblib/callbacks_attachable.rb
@@ -7,22 +7,27 @@ module CallbacksAttachable
     def on(*args, &block)
       __callback_registry__.on(*args, &block)
     end
+    alias on_event on
 
     def once_on(*args, &block)
       __callback_registry__.once_on(*args, &block)
     end
+    alias once_on_event once_on
 
     def until_true_on(*args, &block)
       __callback_registry__.until_true_on(*args, &block)
     end
+    alias until_true_on_event until_true_on
 
     def off(*args)
       __callback_registry__.off(*args)
     end
+    alias off_event off
 
     def trigger(event, *args)
       ObjectSpace.each_object(self).all?{ |inst| inst.trigger(event, *args) }
     end
+    alias trigger_event trigger
 
     private
 
@@ -34,22 +39,27 @@ module CallbacksAttachable
   def on(*args, &block)
     __callback_registry__.on(*args, &block)
   end
+  alias on_event on
 
   def once_on(*args, &block)
     __callback_registry__.once_on(*args, &block)
   end
+  alias once_on_event once_on
 
   def until_true_on(*args, &block)
     __callback_registry__.until_true_on(*args, &block)
   end
+  alias until_true_on_event until_true_on
 
   def off(*args)
     __callback_registry__.off(*args)
   end
+  alias off_event off
 
   def trigger(event, *args)
     self.class.__send__(:__callback_registry__).trigger(self, event, args) and __callback_registry__.trigger(self, event, args)
   end
+  alias trigger_event trigger
 
   private
 
