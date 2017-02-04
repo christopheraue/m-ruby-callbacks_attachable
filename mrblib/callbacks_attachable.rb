1c72debaf4d8898d1365d9aafc4b484845d53ff0
Christopher Aue <mail@christopheraue.net>
2017-02-04 14:22:10 +0100

    refactored CallbacksAttachable module

--- mrblib/callbacks_attachable.rb
+++ mrblib/callbacks_attachable.rb
@@ -1,49 +1,36 @@
 module CallbacksAttachable
+  module RegistryOwnable
+    def on(event, opts = {}, &callback)
+      __callbacks__.register(event, opts, callback)
+    end
+
+    def triggers_on?(event)
+      __callbacks__.registered? event
+    end
+
+    private def __callbacks__
+      @__callbacks__ ||= CallbackRegistry.new(self)
+    end
+  end
+
+  include RegistryOwnable
+
   def self.included(klass)
     klass.extend ClassMethods
   end
 
   module ClassMethods
-    def on(event, opts = {}, &callback)
-      __callback_registry__.register(event, opts, callback)
-    end
-    alias on_event on
+    include RegistryOwnable
 
     def trigger(event, *args)
       ObjectSpace.each_object(self).all?{ |inst| inst.trigger(event, *args) }
     end
-    alias trigger_event trigger
-
-    def triggers_on?(event)
-      __callback_registry__.registered? event
-    end
-
-    private def __callback_registry__
-      @__callback_registry__ ||= CallbackRegistry.new(self)
-    end
   end
-
-  def on(event, opts = {}, &callback)
-    __callback_registry__.register(event, opts, callback)
-  end
-  alias on_event on
 
   def trigger(event, *args)
-    (not @__callback_registry__ or @__callback_registry__.trigger(self, event, args))
-    (@__class_callback_registry__ || __class_callback_registry__).trigger(self, event, args)
+    @__callbacks__ and @__callbacks__.trigger(self, event, args)
+    @class_callbacks ||= self.class.__send__ :__callbacks__
+    @class_callbacks.trigger(self, event, args)
     true
-  end
-  alias trigger_event trigger
-
-  def triggers_on?(event)
-    __callback_registry__.registered? event
-  end
-
-  private def __callback_registry__
-    @__callback_registry__ ||= CallbackRegistry.new(self)
-  end
-
-  private def __class_callback_registry__
-    @__class_callback_registry__ ||= self.class.__send__ :__callback_registry__
   end
 end
