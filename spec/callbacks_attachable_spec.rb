c1cc26fce7ea7145559a4e8d894a539343e3a0b0
Christopher Aue <mail@christopheraue.net>
2017-07-07 13:11:32 +0200

    refactored tests

--- spec/callbacks_attachable_spec.rb
+++ spec/callbacks_attachable_spec.rb
@@ -1,46 +1,26 @@
 require 'spec_helper'
 
-shared_examples_for "executing the callback in its block's scope" do
-  subject { instance.trigger(:event, :arg) }
-  before { expect(callback).to receive(:call).with(self, [:arg]) }
-  it { expect{ subject }.not_to raise_error }
-end
+shared_examples_for "triggering callbacks registered for an instance" do |target|
+  let(:callback) { proc{} }
+  before do
+    callback = self.callback
+    instance.__send__(target).on(:event){ |*args| callback.call(self, args) }
+  end
 
-shared_examples_for "executing the callback in its instance's scope" do
-  subject { instance.trigger(:event, :arg) }
-  before { expect(callback).to receive(:call).with(instance, [:arg]) }
+  before { expect(callback).to receive(:call).with(__send__(target == :class ? :instance : :itself), [:arg]) }
   it { expect{ subject }.not_to raise_error }
 end
 
 describe "CallbacksAttachable included into a regular class" do
-  let!(:klass) { Class.new{ include CallbacksAttachable } }
   subject(:instance) { klass.new }
+  let!(:klass) { Class.new{ include CallbacksAttachable } }
 
-  let(:callback) { proc{} }
+  describe "Triggering callbacks" do
+    subject { instance.trigger(:event, :arg) }
 
-  context "when a callback is attached to the class" do
-    before do
-      callback = self.callback
-      klass.on(:event) { |*args| callback.call(self, args) }
-    end
-
-    it_behaves_like "executing the callback in its instance's scope"
-  end
-
-  context "when a callback is attached to an instance's singleton class" do
-    before { instance.singleton_class.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
-  end
-
-  context "when a callback is attached to an instance" do
-    before { instance.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
+    it_behaves_like "triggering callbacks registered for an instance", :class
+    it_behaves_like "triggering callbacks registered for an instance", :singleton_class
+    it_behaves_like "triggering callbacks registered for an instance", :itself
   end
 end
 
@@ -48,43 +28,21 @@ describe "CallbacksAttachable included into a singleton class" do
   subject(:instance) { Object.new.extend CallbacksAttachable }
   before { instance.singleton_class.class_eval{ include CallbacksAttachable } }
 
-  let(:callback) { proc{} }
+  describe "Triggering callbacks" do
+    subject { instance.trigger(:event, :arg) }
 
-  context "when a callback is attached to the singleton class" do
-    before { instance.singleton_class.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
-  end
-
-  context "when a callback is attached to an instance" do
-    before { instance.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
+    it_behaves_like "triggering callbacks registered for an instance", :singleton_class
+    it_behaves_like "triggering callbacks registered for an instance", :itself
   end
 end
 
 describe "CallbacksAttachable extending an object" do
   subject(:instance) { Object.new.extend CallbacksAttachable }
 
-  let(:callback) { proc{} }
+  describe "Triggering callbacks" do
+    subject { instance.trigger(:event, :arg) }
 
-  context "when a callback is attached to the singleton class" do
-    before { instance.singleton_class.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
-  end
-
-  context "when a callback is attached to an instance" do
-    before { instance.on(:event) do |*args|
-      callback.call(self, args)
-    end }
-
-    it_behaves_like "executing the callback in its block's scope"
+    it_behaves_like "triggering callbacks registered for an instance", :singleton_class
+    it_behaves_like "triggering callbacks registered for an instance", :itself
   end
 end
